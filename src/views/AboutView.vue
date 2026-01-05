4782dabf9a5eb84d09a635aa0e2607940e520f51
cyx <13598067117@163.com>
2026-01-05 18:16:50 +0800

    初始化

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>三方平台</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+   智数平台
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+}
+</script>
